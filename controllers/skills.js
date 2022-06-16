30062d274c85b05011130fdbad1f76ceeaeb34eb
rdokamoto@gmail.com <ryan.okamoto@Ryans-MacBook-Pro.local>
2022-06-16 14:06:33 -0700

    completes part 2 of lab

--- controllers/skills.js
+++ controllers/skills.js
@@ -3,17 +3,37 @@ const Skill = require('../models/skills');
 module.exports = {
     index, 
     show, 
+    new: newSkill, 
+    create, 
+    delete: deleteSkill,
 };
-
+// Index (or show all) conoteroller function
 function index(req, res) {
     res.render('skills/index', {
       skills: Skill.getAll()
     });
   };
 
-//Show controler function
+//Show controller function
 function show(req, res) {
   res.render('skills/show', {
     skill: Skill.getOne(req.params.id),
   })
+}
+
+// New skill conteroller function
+function newSkill(req, res) {
+  res.render('skills/new');
+}
+// Function called to create new skill
+function create(req, res) {
+  Skill.create(req.body);
+  console.log(req.body)
+  res.redirect('/skills');
+  }
+
+// Deletes skill
+function deleteSkill(req, res) {
+  Skill.deleteOne(req.params.id);
+  res.redirect('/skills')
 }
--- routes/skills.js
+++ routes/skills.js
@@ -3,8 +3,15 @@ var router = express.Router();
 
 const skillsCtrl = require('../controllers/skills');
 
+//Skills router paths
 router.get('/', skillsCtrl.index);
 
+router.get('/new', skillsCtrl.new);
+
 router.get('/:id', skillsCtrl.show);
+
+router.post('/', skillsCtrl.create);
+
+router.delete('/:id', skillsCtrl.delete);
 
 module.exports = router;
